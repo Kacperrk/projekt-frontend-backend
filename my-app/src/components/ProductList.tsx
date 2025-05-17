2005b50615e390ad66a2dec97d3b27fcd0d75d5b
Dominika Jedynak <dominikajedynak8@interia.pl>
2025-05-17 22:43:06 +0200

    połączenie ProductList z danymi z Reduxa

--- my-app/src/components/ProductList.tsx
+++ my-app/src/components/ProductList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addToBasket } from '../slices/basketSlice';
+import { RootState } from '../store';
 
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
@@ -9,57 +10,44 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import CardActions from '@mui/material/CardActions';
 
-interface Book {
-  id: number;
-  title: string;
-  author: string;
-  price: number;
-}
-
-const sampleBooks: Book[] = [
-  { id: 1, title: 'Wiedźmin', author: 'Andrzej Sapkowski', price: 45 },
-  { id: 2, title: 'Lalka', author: 'Bolesław Prus', price: 35 },
-  { id: 3, title: 'Pan Tadeusz', author: 'Adam Mickiewicz', price: 30 },
-];
-
 const ProductList: React.FC = () => {
   const dispatch = useDispatch();
+  const products = useSelector((state: RootState) => state.products.items); // ✅ z Reduxa
 
-  const handleAdd = (book: Book) => {
+  const handleAdd = (book: any) => {
     dispatch(addToBasket(book));
   };
 
   return (
-    <Box>
+    <Box sx={{ maxWidth: '1200px', margin: '0 auto', padding: 2 }}>
       <Typography variant="h4" gutterBottom>
         Lista książek
       </Typography>
 
-      <Box
-        display="flex"
-        flexWrap="wrap"
-        gap={2}
-        justifyContent="center"
-      >
-        {sampleBooks.map((book) => (
-          <Card key={book.id} sx={{ width: 300 }}>
-            <CardContent>
-              <Typography variant="h6">{book.title}</Typography>
-              <Typography color="text.secondary">{book.author}</Typography>
-              <Typography variant="body2">{book.price} zł</Typography>
-            </CardContent>
-            <CardActions>
-              <Button
-                size="small"
-                variant="contained"
-                onClick={() => handleAdd(book)}
-              >
-                Dodaj do koszyka
-              </Button>
-            </CardActions>
-          </Card>
-        ))}
-      </Box>
+      {products.length === 0 ? (
+        <Typography color="text.secondary">Brak dostępnych książek.</Typography>
+      ) : (
+        <Box display="flex" flexWrap="wrap" gap={2} justifyContent="center">
+          {products.map((book) => (
+            <Card key={book.id} sx={{ width: 300 }}>
+              <CardContent>
+                <Typography variant="h6">{book.title}</Typography>
+                <Typography color="text.secondary">{book.author}</Typography>
+                <Typography variant="body2">{book.price} zł</Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  size="small"
+                  variant="contained"
+                  onClick={() => handleAdd(book)}
+                >
+                  Dodaj do koszyka
+                </Button>
+              </CardActions>
+            </Card>
+          ))}
+        </Box>
+      )}
     </Box>
   );
 };
