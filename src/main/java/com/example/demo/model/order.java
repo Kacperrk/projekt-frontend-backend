6c67ea36b6063480da1d31ef9ca5367b94405458
JNSau <148083836+JNSau@users.noreply.github.com>
2025-05-29 19:46:19 +0200

    Create order.java

---
+++ src/main/java/com/example/demo/model/order.java
@@ -0,0 +1,57 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "order_date", nullable = false)
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private OrderStatus status;
+
+    @Column(name = "total_price", nullable = false)
+    private BigDecimal totalPrice;
+
+    @Column(name = "street", nullable = false, length = 100)
+    private String street;
+
+    @Column(name = "building_number", nullable = false, length = 10)
+    private String buildingNumber;
+
+    @Column(name = "apartment_number", length = 10)
+    private String apartmentNumber;
+
+    @Column(name = "postal_code", nullable = false, length = 10)
+    private String postalCode;
+
+    @Column(name = "city", nullable = false, length = 100)
+    private String city;
+
+    @Column(name = "country", nullable = false, length = 100)
+    private String country;
+
+    @Column(name = "archived", nullable = false)
+    private boolean archived;
+}
