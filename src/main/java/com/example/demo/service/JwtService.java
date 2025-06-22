e81fdb1e1532a7c84da08a7c65f62bd4d8a65781
Kacperrk <kacper0492@wp.pl>
2025-06-22 18:36:29 +0200

    add secret token jwt and google

--- src/main/java/com/example/demo/service/JwtService.java
+++ src/main/java/com/example/demo/service/JwtService.java
@@ -10,14 +10,16 @@ import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import org.springframework.stereotype.Service;
 import com.example.demo.model.User;
+import org.springframework.beans.factory.annotation.Value;
 
 @Service
 public class JwtService {
 
-    // Secret key for signing JWT (in a real application, use a secure key and keep it safe)
-    private static final String SECRET_KEY = "MySuperSecretKeyForJWTGeneration";
-    // Token validity period (e.g., 24 hours)
-    private static final long EXPIRATION_TIME_MS = 24 * 60 * 60 * 1000;
+    @Value("${app.jwt.secret}")
+    private String SECRET_KEY;
+
+    @Value("${app.jwt.expires}")
+    private long EXPIRATION_TIME_MS;
 
     public String generateToken(User user) {
         Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
