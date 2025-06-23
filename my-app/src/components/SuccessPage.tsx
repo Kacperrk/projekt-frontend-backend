c4c7194cb62ec6e93b02aa2fe0c93ffd7e28368d
Kacperrk <kacper0492@wp.pl>
2025-06-23 07:29:09 +0200

    fix success payment page

--- my-app/src/components/SuccessPage.tsx
+++ my-app/src/components/SuccessPage.tsx
@@ -1,20 +1,17 @@
-import {useLocation} from "react-router-dom";
-import {useEffect} from "react";
-import {toast} from "react-toastify";
-import CancelPage from "./CancelPage";
-
-function Congratulations() {
-    return null;
-}
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 
 const SuccessPage = () => {
     const { search } = useLocation();
     const sessionId = new URLSearchParams(search).get('session_id');
+
     useEffect(() => {
-        fetch(`/api/orders/confirm/${sessionId}`);  // opcjonalne dociągnięcie statusu
+        fetch(`/api/orders/confirm/${sessionId}`);
         toast.success('Płatność zakończona sukcesem!');
     }, []);
-    return <Congratulations />;
+
+    return <div>Płatność zakończona sukcesem!</div>;
 };
 
 export default SuccessPage;
