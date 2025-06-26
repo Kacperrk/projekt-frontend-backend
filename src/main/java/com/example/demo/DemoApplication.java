509cc03af1cb1b98b08f986e6fbce80e7bfffceb
Kacperrk <kacper0492@wp.pl>
2025-06-26 22:58:32 +0200

    secrets keys in .env

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,24 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                .load();
+
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+
+        System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+
+        System.setProperty("STRIPE_SECRET_KEY", dotenv.get("STRIPE_SECRET_KEY"));
+
         SpringApplication.run(DemoApplication.class, args);
     }
 }
